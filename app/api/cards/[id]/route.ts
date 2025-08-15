45ece7394e1158e8d65d7928f4d9273c4b8b3da6
Ruben Serra <102155988+rserravi@users.noreply.github.com>
2025-08-15 11:12:43 +0200

    refactor: use shared Prisma instance in API routes

--- app/api/cards/[id]/route.ts
+++ app/api/cards/[id]/route.ts
@@ -1,8 +1,6 @@
 
 import { NextRequest, NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { prisma } from '@/lib/db'
 
 export const dynamic = 'force-dynamic'
 
